0827be51a6de9bea778c4287593e49fcd5ce692c
Dave W <dviper@gmail.com>
2017-09-09 14:31:55 -0600

    add clubpage import and route

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Header from '../../Components/Header/Header';
 import LoginForm from '../../Components/LoginForm/LoginForm';
 import SignUpForm from '../../Components/SignUpForm/SignUpForm';
+import ClubPage from '../../Components/ClubPage/ClubPage';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Route } from 'react-router-dom';
 
@@ -13,6 +14,7 @@ class App extends Component {
           <section>
             <Route exact path="/" component={LoginForm} />
             <Route exact path="/signup" component={SignUpForm} />
+            <Route exact path="/clubpage" component={ClubPage} />
           </section>
         </Router>
       </div>

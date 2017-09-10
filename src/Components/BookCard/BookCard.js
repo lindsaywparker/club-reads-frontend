c96270a39a181e244a50b102089f580cbb35a4cf
Dave W <dviper@gmail.com>
2017-09-10 15:11:28 -0600

    Add post fetch request for suggest button

--- src/Components/BookCard/BookCard.js
+++ src/Components/BookCard/BookCard.js
@@ -1,22 +1,45 @@
 import React from 'react';
 
 const BookCard = ({ book }) => {
+  this.addBookToDB = (book) => {
+    fetch('/api/v1/book', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        title: book.title,
+        author: book.authors,
+        status: 'suggested',
+        downvotes: '0',
+        upvotes: '0',
+        image: book.image_url,
+        user_id: 1,
+        ISBN: 123456789,
+        description: 'it was okay',
+      }),
+    })
+      .then(res => res.json())
+      .then(data => console.log(data)) // TODO: add sucess message to DOM
+      .catch(err => console.log(err));
+  };
+
   return (
     <div className="book-card-component">
       <img src={book.image_url} alt="Book Cover" />
       <div className="book-info">
         <p>Title: {book.title}</p>
-        <p>Author: {book.authors[0]}</p>
+        <p>Author: {book.authors}</p>
         <p>Rating: {book.avg_rating}</p>
         <p>Number of Ratings: {book.ratings_count}</p>
         <p>Description: Hi Im a description</p>
       </div>
       <a href={`https://www.goodreads.com/book/show/${book.book_id}`} target="_blank">View on Goodreads</a>
-      <button onClick={() => console.log('You clicked suggest! Good for you!')}>
+      <button onClick={() => this.addBookToDB(book)}>
         Suggest
       </button>
     </div>
-  )
+  );
 };
 
 export default BookCard;

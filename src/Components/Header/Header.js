6e32dc4b3c7ce0a1206a1fd9f7f5b2077ba9149f
Dave W <dviper@gmail.com>
2017-09-12 16:01:56 -0600

    Add onClick fn for Logout, conditionally render nav links

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom'
 
-const Header = props => (
-  <header className="header-component">
-    <h1 className="logo">club<span className="logo-accent">reads</span></h1>
+const Header = (props) => {
+
+  const logoutHandler = () => {
+    props.getUserId(null, null);
+    props.history.push('/');
+  };
+
+  const loggedInLinks = (
     <nav className="nav-link-container">
       <NavLink to={`/clubpage/${props.clubId}`} className="nav-link" activeClassName="selected">Club Page</NavLink>
       <NavLink to="/suggestbook" className="nav-link" activeClassName="selected">Suggest a Book</NavLink>
-      <a className="nav-link" href="#">Logout</a>
-      <NavLink to="/" className="nav-link" activeClassName="selected">Login</NavLink>
+      <span role="link" tabIndex={0} onClick={() => logoutHandler()} className="nav-link">Logout</span>
     </nav>
-  </header>
-);
+  );
+
+  return (
+    <header className="header-component">
+      <h1 className="logo">club<span className="logo-accent">reads</span></h1>
+      {props.clubId ? loggedInLinks : null}
+    </header>
+  );
+};
 
 export default Header;

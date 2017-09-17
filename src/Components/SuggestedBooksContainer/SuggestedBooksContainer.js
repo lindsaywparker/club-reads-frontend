ce919aa39b5d37fc8c901ef1213bb06191d4f82c
Lindsay Parker <lindsay.w.parker@gmail.com>
2017-09-17 17:09:08 -0600

    Modify state to 'suggestedBooks' and filter all club books for mapping

--- src/Components/SuggestedBooksContainer/SuggestedBooksContainer.js
+++ src/Components/SuggestedBooksContainer/SuggestedBooksContainer.js
@@ -6,19 +6,22 @@ export default class SuggestedBooksContainer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      books: [],
+      suggestedBooks: [],
     };
   }
 
   componentDidMount() {
     fetch(`/api/v1/book?club_id=${this.props.clubId}`)
       .then(res => res.json())
-      .then(books => this.setState({ books }))
+      .then((books) => {
+        const suggestedBooks = books.filter(book => book.status === 'suggested');
+        this.setState({ suggestedBooks });
+      })
       .catch(err => console.log({ err }));
   }
 
   render() {
-    const bookResults = this.state.books.map(book => (
+    const bookResults = this.state.suggestedBooks.map(book => (
       <BookCard
         key={book.goodreads_id}
         book={book}

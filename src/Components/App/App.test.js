9e558a5ae46339269dadd46c30aa1829fda66759
Travis <tlgreg86@gmail.com>
2017-09-19 15:30:16 -0600

    Add initial test setup and check for component rendering

--- src/Components/App/App.test.js
+++ src/Components/App/App.test.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { mount, shallow } from 'enzyme';
+import fetchMock from 'fetch-mock';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+describe('APP COMPONENT', () => {
+  const wrapper = shallow(<App />);
+
+  const resolveAfter2Seconds = () => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve();
+      }, 2000);
+    });
+  };
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+  });
+
 });

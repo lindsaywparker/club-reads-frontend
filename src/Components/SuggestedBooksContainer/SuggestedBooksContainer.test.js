3073f984f7fe571b5cb3a240549f472a23b737cd
Lindsay Parker <lindsay.w.parker@gmail.com>
2017-09-19 17:05:17 -0600

    Suggested Books Container Component testing skeleton

---
+++ src/Components/SuggestedBooksContainer/SuggestedBooksContainer.test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+import fetchMock from 'fetch-mock';
+import SuggestedBooksContainer from './SuggestedBooksContainer';
+
+describe('SUGGESTED BOOKS CONTAINER COMPONENT', () => {
+  const wrapper = shallow(<SuggestedBooksContainer />);
+
+  const resolveAfter2Seconds = () =>
+    new Promise((resolve) => {
+      setTimeout(() => {
+        resolve();
+      }, 2000);
+    });
+    
+
+  it('should fetch books and update state', async () => {
+    fetchMock.get('/api/v1/book?club_id=1', []);
+    wrapper.instance().getUserId(1, 1);
+    await resolveAfter2Seconds();
+
+    expect(wrapper.state()).toEqual();
+  });
+});

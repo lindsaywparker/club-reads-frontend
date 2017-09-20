f1a027234407e69facec984abdd99158b3782d28
Lindsay Parker <lindsay.w.parker@gmail.com>
2017-09-19 19:53:11 -0600

    Add 'should render' test for ClubPage

--- src/Components/ClubPage/ClubPage.test.js
+++ src/Components/ClubPage/ClubPage.test.js
@@ -5,4 +5,8 @@ import ClubPage from './ClubPage';
 describe('CLUB PAGE COMPONENT', () => {
   const wrapper = shallow(<ClubPage readBooks={[{}, {}]} currentBook={{}} userInfo={{}} match={{ params: { club_id: 1 } }} history={{ location: { pathname: '' } }} apiUrl={''} />);
 
+  it('should render a component element', () => {
+    expect(wrapper.find('.club-page-component')).toHaveLength(1);
+  });
+
 });

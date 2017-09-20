6e8083a6e6059a4164f6865a1f852d841086f2f4
Lindsay Parker <lindsay.w.parker@gmail.com>
2017-09-19 19:31:12 -0600

    Housekeeping

--- src/Components/SuggestedBooksContainer/SuggestedBooksContainer.test.js
+++ src/Components/SuggestedBooksContainer/SuggestedBooksContainer.test.js
@@ -1,18 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { shallow } from 'enzyme';
-import fetchMock from 'fetch-mock';
 import SuggestedBooksContainer from './SuggestedBooksContainer';
 
 describe('SUGGESTED BOOKS CONTAINER COMPONENT', () => {
   const wrapper = shallow(<SuggestedBooksContainer />);
-
-  const resolveAfter2Seconds = () =>
-    new Promise((resolve) => {
-      setTimeout(() => {
-        resolve();
-      }, 2000);
-    });
 
   it('should have initial state', () => {
     expect(wrapper.state()).toEqual({
@@ -20,10 +11,6 @@ describe('SUGGESTED BOOKS CONTAINER COMPONENT', () => {
     });
   });
 
-  it.skip('should fetch books and update state', async () => {
-    fetchMock.get('/api/v1/book?club_id=1', []);
-    wrapper.instance().getUserId(1, 1);
-    await resolveAfter2Seconds();
   it('should render a component element', () => {
     expect(wrapper.find('.suggested-books-container')).toHaveLength(1);
     expect(wrapper.find('.suggestion-instructions')).toHaveLength(2);
@@ -84,6 +71,6 @@ describe('SUGGESTED BOOKS CONTAINER COMPONENT', () => {
       ],
     });
 
-    expect(wrapper.state()).toEqual();
+    expect(wrapper.find('BookCard')).toHaveLength(3);
   });
 });

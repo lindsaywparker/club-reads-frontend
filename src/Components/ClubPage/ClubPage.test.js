ac30f5bcf62494de705c1b0fae0f68492fcbf682
Lindsay Parker <lindsay.w.parker@gmail.com>
2017-09-19 19:52:42 -0600

    Add ClubPage component test skeleton

---
+++ src/Components/ClubPage/ClubPage.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import ClubPage from './ClubPage';
+
+describe('CLUB PAGE COMPONENT', () => {
+  const wrapper = shallow(<ClubPage readBooks={[{}, {}]} currentBook={{}} userInfo={{}} match={{ params: { club_id: 1 } }} history={{ location: { pathname: '' } }} apiUrl={''} />);
+
+});

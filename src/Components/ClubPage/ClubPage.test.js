fd83f69e72d71c251dc315485a404f3498a16e41
Lindsay Parker <lindsay.w.parker@gmail.com>
2017-09-19 19:53:42 -0600

    Add Carousel rendering tests for ClubPage

--- src/Components/ClubPage/ClubPage.test.js
+++ src/Components/ClubPage/ClubPage.test.js
@@ -9,4 +9,13 @@ describe('CLUB PAGE COMPONENT', () => {
     expect(wrapper.find('.club-page-component')).toHaveLength(1);
   });
 
+  it('should not render the Carousel if there are no books', () => {
+    const wrapperEmpty = shallow(<ClubPage readBooks={[]} currentBook={null} userInfo={{}} match={{ params: { club_id: 1 } }} history={{ location: { pathname: '' } }} apiUrl={''} />);
+    expect(wrapperEmpty.find('Carousel')).toHaveLength(0);
+  });
+
+  it('should render the Carousel if there are books', () => {
+    expect(wrapper.find('Carousel')).toHaveLength(1);
+  });
+
 });

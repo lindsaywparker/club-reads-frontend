16551536fdaec1bd625aa51d4a3072a518edca70
Lindsay Parker <lindsay.w.parker@gmail.com>
2017-09-20 10:23:57 -0600

    Housekeeping

--- src/Components/SearchResultsContainer/SearchResultsContainer.js
+++ src/Components/SearchResultsContainer/SearchResultsContainer.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Header from '../Header/Header';
 import BookCard from '../BookCard/BookCard';
 
 const SearchResultsContainer = ({ books, userInfo, pathname, suggestedBooks, apiUrl }) => {
